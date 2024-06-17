7553a96a7bef218e9f8f2b05b8fc0ff892f2b69b
Sahil Bhosale <134532505+Sahil110102@users.noreply.github.com>
2024-06-17 12:23:19 +0530

    Sort Character by Frequency

---
+++ STRINGS/Medium/SortCharFreq.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * SortCharFreq
+ */
+public class SortCharFreq {
+
+    public String frequencySort(String s) {
+
+        StringBuilder ans = new StringBuilder();
+        Map <Character , Integer > hm = new HashMap<>();
+
+        for(char ch : s.toCharArray()){
+            hm.put(ch , hm.getOrDefault(ch , 0) + 1);
+        }    
+        List<Character> list = new ArrayList(hm.keySet());
+        list.sort((ob1 , ob2) -> hm.get(ob2) - hm.get(ob1));
+        for( char ch : list ){
+            for(int i=0 ; i< hm.get(ch); i++){
+                ans.append(ch);
+            }
+        }  
+        return ans.toString() ;
+    }
+}
